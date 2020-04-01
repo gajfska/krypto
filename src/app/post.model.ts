45c464e1e7de0e54b936ba53630dcfc167642ef1
Werka <weronika.gajska@gmail.com>
2020-04-01 11:32:13 +0200

    basic files

---
+++ src/app/post.model.ts
@@ -0,0 +1,15 @@
+export interface Post {
+  date: number;
+  price?: number;
+  // high: number;
+  // low: number;
+  // close: string;
+  // volume: number;
+  // closeTime: number;
+  // quoteAssetVolume: number;
+  // numberOfTrades: number;
+  // takerBuyBaseAssetVolume: number;
+  // takerBuyQuoteAssetVolume: number;
+  // canBeIgnored: number;
+  // id?: string;
+}
